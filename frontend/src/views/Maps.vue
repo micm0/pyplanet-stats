8c303c8692da92290e7333ea554795de30249986
El Mezrichi Monir <monir.elmezrichi@hotmail.com>
2021-01-18 04:46:01 +0100

    init vuejs typescript frontend

---
+++ frontend/src/views/Maps.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="maps">
+    Here will be tm maps
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
+
+@Component
+export default class Maps extends Vue {
+  maps = undefined;
+
+  mounted() {
+    Vue.axios.get("http://localhost:3000/api/maps/").then(resp => {
+      this.maps = resp.data;
+    });
+  }
+}
+</script>

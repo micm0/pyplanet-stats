568c9221f043e3b623d082cc3a20b871951bff8b
El Mezrichi Monir <monir.elmezrichi@hotmail.com>
2021-01-26 09:50:02 +0100

    Header with map informations in MapRecords added

--- frontend/src/views/MapRecords.vue
+++ frontend/src/views/MapRecords.vue
@@ -1,11 +1,119 @@
 <template>
   <div>
+    <v-row class="mb-2">
+      <v-col cols="3">
+        <v-card elevation="10">
+          <v-list dense>
+            <v-list-item>
+              <v-list-item-title class="text-left">Name</v-list-item-title>
+              <v-list-item-title
+                v-html="tmStyle(track.name)"
+              ></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="text-left"
+                >Author login</v-list-item-title
+              >
+              <v-list-item-title
+                v-text="track.author_login"
+              ></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="text-left"
+                >Author time</v-list-item-title
+              >
+              <v-list-item-title
+                v-html="toTmTime(track.time_author)"
+              ></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="text-left"
+                >Environment</v-list-item-title
+              >
+              <v-list-item-title v-text="track.environment"></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="text-left">UID</v-list-item-title>
+              <span class="caption">
+                {{ track.uid }}
+              </span>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card elevation="10">
+          <v-subheader>Karma Rating</v-subheader>
+          <v-rating
+            empty-icon="mdi-heart-outline"
+            full-icon="mdi-heart"
+            half-icon="mdi-heart-half-full"
+            v-model="trackKarmaOfFive"
+            half-increments
+            background-color="grey"
+            color="primary"
+            readonly
+            dense
+          ></v-rating>
+          <p class="grey--text text--lighten-2 caption">
+            karma: {{ trackKarmaOfFive }} /5
+          </p>
+          <p class="grey--text text--lighten-2 caption ml-2">
+            votes numbers: {{ trackKarmaCount }}
+          </p>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card>
+          <v-subheader>Mania-Exchange Informations</v-subheader>
+          <v-list v-if="track.mx_id" dense>
+            <v-list-item>
+              <v-list-item-title class="text-left">
+                Link
+              </v-list-item-title>
+              <v-list-item-title
+                ><v-btn
+                  v-if="track.mx_id"
+                  :href="mxLink(track.mx_id)"
+                  target="blank"
+                  small
+                  text
+                  fab
+                >
+                  <img src="../assets/mx_full.png" width="30px" /> </v-btn
+              ></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="text-left">
+                Download
+              </v-list-item-title>
+              <v-list-item-title>
+                <v-btn
+                  small
+                  color="indigo"
+                  :href="
+                    `https://tm.mania-exchange.com/maps/download/${track.mx_id}`
+                  "
+                  ><v-icon left> mdi-download </v-icon>Download</v-btn
+                >
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+          <span v-else class="caption">not on mania-exchange</span>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card width="300px">
+          <v-img :src="mxThumb(track.mx_id)" alt="map thumbnail" />
+        </v-card>
+      </v-col>
+    </v-row>
     <p>
       Server records on
       <span v-html="tmStyle(track.name)" class="mx-1"></span> by
       {{ track.author_login }}
     </p>
-    <v-card>
+    <v-card elevation="10">
       <v-card-title>
         <v-text-field
           v-model="search"
@@ -80,6 +188,9 @@ export default class TrackRecords extends Vue {
     mx_id: null
   };
   trackRecords: TrackRecord[] = [];
+  trackKarma = 0;
+  trackKarmaOfFive = 0;
+  trackKarmaCount = 0;
   headers = [
     { text: "Rank", align: "start", value: "rank" },
     { text: "Player", value: "player_nickname", sortable: false },
@@ -97,7 +208,38 @@ export default class TrackRecords extends Vue {
       .then(resp => {
         this.track = resp.data;
         this.emitTrackName();
+        console.log(this.track);
       });
+
+    Vue.axios
+      .get(`http://localhost:3000/api/karma/${this.$route.params.id}`)
+      .then(resp => {
+        /* eslint-disable @typescript-eslint/no-unused-vars */
+        let totalScore = 0;
+        let totalAbs = 0;
+        let trackKarmaNegative = 0;
+        let trackKarmaPositive = 0;
+        this.trackKarmaCount = resp.data.length;
+
+        resp.data.forEach((karma: { score: number }) => {
+          const score = karma.score;
+          totalScore += score;
+          totalAbs += Math.abs(score);
+          if (score > 0) trackKarmaPositive += score;
+        });
+
+        trackKarmaNegative = totalAbs - trackKarmaPositive;
+        this.trackKarma = totalScore;
+        if (trackKarmaPositive > 0)
+          this.trackKarmaOfFive = (100 * (trackKarmaPositive / totalAbs)) / 20;
+
+        //round at most 2 decimal places
+        this.trackKarmaOfFive = Math.round(this.trackKarmaOfFive * 100) / 100;
+        //TODO: list of voters with nickname and vote
+      });
+  }
+  copyUid() {
+    navigator.clipboard.writeText(this.track.uid);
   }
   tmStyle(name: string): string {
     return MPStyle(name);
@@ -107,6 +249,12 @@ export default class TrackRecords extends Vue {
   }
   formatDate(updatedAt: string): string {
     return dayjs(updatedAt).format("DD/MM/YYYY | HH:mm");
+  }
+  mxLink(mxId: number): string {
+    return "https://tm.mania-exchange.com/maps/" + mxId;
+  }
+  mxThumb(mxId: number): string {
+    return "https://tm.mania-exchange.com/maps/thumbnail/" + mxId;
   }
 }
 </script>
